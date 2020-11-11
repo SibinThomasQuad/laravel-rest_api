a8bd6b3dbeb307ec9bdb3c87c5b5cb4b19de33d8
ICS05 <sibinthomasmdkm@gmail.com>
2020-11-11 17:43:21 +0530

    laravel rest api commit

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    // ...
+});
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+
+    // ...
+
+    // public routes
+    Route::post('/login', 'App\Http\Controllers\Auth\ApiAuthController@login')->name('login.api');
+    Route::post('/register','App\Http\Controllers\Auth\ApiAuthController@register')->name('register.api');
+    Route::post('/logout', 'App\Http\Controllers\Auth\ApiAuthController@logout')->name('logout.api');
+
+    // ...
+
+});
+
